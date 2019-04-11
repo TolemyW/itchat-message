84a8c5a0e40ae622ef45910317b1833487447412
CorkiZhang <46222967+CorkiZhang@users.noreply.github.com>
2019-04-12 07:34:23 +0800

    Delete test.sh

--- test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-git config --global user.email "1729795578@qq.com"
-git config --global user.name "CorkiZhang"
-git config --global push.default simple
-git add -A
-git commit -m '.'
-git push origin
