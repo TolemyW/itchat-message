efae7ac9d69a39e9d291176f89d3a059bbcd7774
CorkiZhang <1729795578@qq.com>
2019-04-11 20:46:01 +0800

    .

--- test.sh
+++ test.sh
@@ -2,6 +2,6 @@
 git config --global user.email "1729795578@qq.com"
 git config --global user.name "CorkiZhang"
 git config --global push.default simple
-git add .
+git add -A
 git commit -m '.'
 git push origin
